923f7b152d03882496f7a190b8e12c175dcfadc5
Rukesh R <Rukeshr16@gmail.com>
2020-09-19 10:56:31 +0530

    Added listing article page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show] #this will give all articles routes
+  resources :articles, only: [:show , :index] #this will give all articles routes
 
 end
