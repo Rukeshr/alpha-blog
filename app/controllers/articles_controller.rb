151d8ecab289e65e79eb7aade4aa1da23d958d33
Rukesh R <Rukeshr16@gmail.com>
2020-09-19 18:33:21 +0530

    Bootstrap commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,9 @@
 class ArticlesController < ApplicationController
-
-    def show 
-        @article = Article.find(params[:id])
+    before_action :common_article, only: [:show, :edit, :update, :destroy] 
+ 
+    def show    
     end
+    
     def index
         @articles = Article.all
     end
@@ -10,10 +11,10 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     def edit
-        @article = Article.find(params[:id])
+        
     end
     def create
-        @article = Article.new(params.require(:article).permit(:title,:description))
+        @article = Article.new(common_params)
         if @article.save
             flash[:notice] = "Article was created successfully."
         redirect_to @article
@@ -22,8 +23,8 @@ class ArticlesController < ApplicationController
         end
     end
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title,:description))
+        
+        if @article.update(common_params)
             flash[:notice] = "Article was updated successfully."  
             redirect_to @article
         else
@@ -32,10 +33,21 @@ class ArticlesController < ApplicationController
         end
 
         def destroy
-            @article = Article.find(params[:id])
+            
             @article.destroy
             redirect_to articles_path
 
         end
+        #DRY tech is using here
+        private
+        def common_article
+            @article = Article.find(params[:id])
+        end
+
+        def common_params
+            params.require(:article).permit(:title,:description)
+        end
+
+
 
 end
