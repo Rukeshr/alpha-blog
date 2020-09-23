d253cf066be009172cd4d29b9f35e818362bde58
Rukesh R <Rukeshr16@gmail.com>
2020-09-23 14:38:44 +0530

    SignUp page added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles #this will give all articles routes
-
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
