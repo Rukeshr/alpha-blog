482bec4244cbb08a5b3efc47c3df271ee550decd
Rukesh R <Rukeshr16@gmail.com>
2020-09-26 17:48:53 +0530

    added session controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources :articles #this will give all articles routes
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
 end
